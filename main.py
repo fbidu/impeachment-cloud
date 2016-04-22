cb1c2279a510e6a3eb5d8848b7d4f6fe021d963d
Felipe Rodrigues <felipe@felipevr.com>
2016-04-22 17:02:08 -0300

    Adding more analysis

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ votes = csv.reader(
 
 votes_yes = []
 votes_no = []
+votes_men = []
+votes_women = []
 
 for row in votes:
     if row[3].lower() == 'sim':
         votes_yes.append(row[5])
-
     else:
         votes_no.append(row[5])
 
+    if row[4].lower() == 'm':
+        votes_men.append(row[5])
+    else:
+        votes_women.append(row[5])
+
 votes_yes = ' '.join(votes_yes)
 votes_no = ' '.join(votes_no)
-
+votes_men = ' '.join(votes_men)
+votes_women = ' '.join(votes_women)
+print(votes_men)
 WordCloud(width=1080, height=720).generate(votes_yes).to_file('yes.png')
 WordCloud(width=1080, height=720).generate(votes_no).to_file('no.png')
+WordCloud(width=1080, height=720).generate(votes_men).to_file('men.png')
+WordCloud(width=1080, height=720).generate(votes_women).to_file('women.png')
